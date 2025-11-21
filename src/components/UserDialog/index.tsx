9752d56f7151aed48b6b9c78407aa764a2201d2d
Thomaz <122468659+Th0mzzz@users.noreply.github.com>
2025-11-21 20:19:45 -0300

    Update index.tsx

--- src/components/UserDialog/index.tsx
+++ src/components/UserDialog/index.tsx
@@ -45,7 +45,7 @@ export default function UserDialog({user, onClose, open}: { user?: User, open: b
     return (
         <>
             <Dialog open={open} onClose={onClose}
-                    className={"flex items-center justify-center w-full px-4 py-8 fixed z-50 inset-0 overflow-y-auto bg-black/30 text-[var(--text)]"}>
+                    className={"flex justify-center w-full px-4 py-8 fixed z-50 inset-0 overflow-y-auto bg-black/30 text-[var(--text)]"}>
 
                 <DialogPanel
                     className="relative w-full max-w-[var(--max-content)] px-8 py-4 border border-[var(--border-color)] bg-[var(--surface)] rounded-lg shadow-lg transition duration-300 ease-out data-closed:opacity-0">
